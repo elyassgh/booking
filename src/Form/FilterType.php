82f70312400ccdc357aad052444808333709fa66
Ilyass Elghazi <61422528+elyassgh@users.noreply.github.com>
2021-02-01 00:50:46 +0100

    Update FilterType.php

--- src/Form/FilterType.php
+++ src/Form/FilterType.php
@@ -22,31 +22,35 @@ class FilterType extends AbstractType
                     "3 Stars" => 3,
                     "4 Stars" => 4,
                     "5 Stars" => 5,
+                    "6 Stars" => 6,
                 ]
             ])
             ->add('type' , ChoiceType::class , [
                 'choices' => [
-                    'Luxury Room' => 'Luxury room',
-                    'Deluxe Room' => 'Deluxe room',
-                    'Executive Room' => 'Executive room',
-                    'Small Room' => 'Small room',
+                    'Single room'=> 'Single room',
+                    'King room' => 'King room',
+                    'Luxury room' => 'Luxury room',
+                    'Deluxe room' => 'Deluxe room',
+                    'Twin room' => 'Twin room',
+                    'Studio' => 'Studio'
                 ]
             ])
             ->add('distance' , ChoiceType::class , [
                 'choices' => [
                     'City Center' => 0,
-                    'Less than 1 Km from center' => 1,
                     'Less than 3 Km from center' => 3,
                     'Less than 5 Km from center' => 5,
+                    'Less than 10 Km from center' => 10,
+                    'Less than 20 Km from center' => 20,
                 ]
             ])
             ->add('maxprice' , IntegerType::class , [
                 'attr' => [
                     "data-provide" => "slider",
-                    "data-slider-ticks" => "[150, 500, 750, 1000]",
-                    "data-slider-ticks-labels" => '["150€", "500€", "750€", "1000€"]',
-                    "data-slider-step" => "10",
-                    "data-slider-value" => "500",
+                    "data-slider-ticks" => "[50, 250, 500, 1000]",
+                    "data-slider-ticks-labels" => '["50€", "250€", "500€", "1000€"]',
+                    "data-slider-step" => "50",
+                    "data-slider-value" => "250",
                 ]
             ])
         ;
